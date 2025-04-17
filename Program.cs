6c4395830f4aa125d125ca1c419d1163ade01f3b
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-04-16 22:34:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baze;
+            int altura;
+            int area;
+
+            Console.Write("informe o valor da base do retangulo");
+            baze = int.Parse(Console.ReadLine());
+
+            Console.Write("informe o valor da altura do retangulo");
+            altura = int.Parse(Console.ReadLine());
+
+            area = baze * altura;
+            Console.WriteLine("o valor da area de retangulo de baze {0} e altura {1} e area {2}", baze, altura, area);
+        }
+    }
+}
