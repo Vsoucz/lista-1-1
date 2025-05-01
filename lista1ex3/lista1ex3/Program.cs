e54c8f997cc3595001cc2f6f374f4ab647ab75de
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-04-30 22:17:21 -0300

    Add files via upload

---
+++ lista1ex3/lista1ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal;
+             double area;
+
+            Console.Write("digite valor da diagonal");
+            diagonal =
+
+                double.Parse(Console.ReadLine());
+
+          area = diagonal * diagonal / 2;
+            Console.WriteLine("o valor da area do quadrado de diagonal {0} e area {1}", diagonal, area);
+          
+        }
+    }
+}
