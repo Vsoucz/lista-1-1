382c2b92cde5cd2d68a42eca0d1e8480e82db53b
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 20:00:29 -0300

    Add files via upload

---
+++ lista1exr7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exr7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double milhaNautica = 1852;
+            Console.Write("Milhas marítimas: ");
+            double milhas = double.Parse(Console.ReadLine());
+            double km = milhas * milhaNautica / 1000;
+            Console.WriteLine($"{milhas} milhas marítimas = {km} km");
+
+        }
+    }
+}
