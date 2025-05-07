f1c16e3ccd07fb97c9af56713f9f47ab8562bf25
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 20:04:12 -0300

    Add files via upload

---
+++ lista1exr9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exr9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Diâmetro do círculo: ");
+            double d = double.Parse(Console.ReadLine());
+            double r = d / 2;
+            double area = Math.PI * r * r;
+            Console.WriteLine($"Área do círculo: {area}");
+
+        }
+    }
+}
