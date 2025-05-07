8ff72c5ddb038cc8a0393e1fc22cb4b6c8a6e62e
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 20:09:19 -0300

    Add files via upload

---
+++ lista1exr10/lista1exr10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exr10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em dólares: ");
+            double dolares = double.Parse(Console.ReadLine());
+            double reais = cotacao * dolares;
+            Console.WriteLine($"Valor em reais: R$ {reais}");
+
+        }
+    }
+}
