7b77733e984f5e946e09e4ab7328f1e0bf6f5899
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 19:56:36 -0300

    Add files via upload

---
+++ lista1exr5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exr5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write($"Valor {i}: ");
+                soma += double.Parse(Console.ReadLine());
+            }
+            double media = soma / 4;
+            Console.WriteLine($"Média aritmética: {media}");
+
+        }
+    }
+}
