549a708541b2eacd11e645b599d2306653b256d6
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 20:02:42 -0300

    Add files via upload

---
+++ lista1exr8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exr8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperatura em °C: ");
+            double c = double.Parse(Console.ReadLine());
+            double f = (c * 9 / 5) + 32;
+            Console.WriteLine($"Temperatura em Fahrenheit: {f}");
+
+        }
+    }
+}
