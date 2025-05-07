cf43294ec2146c2ffb102ded0f7fb2da2d161a21
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 19:52:12 -0300

    Add files via upload

---
+++ lista1erx6/lista1erx6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1erx6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Valor 1: ");
+            double v1 = double.Parse(Console.ReadLine());
+            Console.Write("Valor 2: ");
+            double v2 = double.Parse(Console.ReadLine());
+            double mediaGeom = Math.Sqrt(v1 * v2);
+            Console.WriteLine($"Média geométrica: {mediaGeom}");
+
+        }
+    }
+}
