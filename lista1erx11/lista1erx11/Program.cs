49d394b74caccc3fecf8bd87e27fae3a1ffd9fd2
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-05-07 20:09:43 -0300

    Add files via upload

---
+++ lista1erx11/lista1erx11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1erx11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Valor de X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Valor de Y: ");
+            double y = double.Parse(Console.ReadLine());
+            double resultado = Math.Pow(x, y);
+            Console.WriteLine($"X elevado a Y = {resultado}");
+
+        }
+    }
+}
